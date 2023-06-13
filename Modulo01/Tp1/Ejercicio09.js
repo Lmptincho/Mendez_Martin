ab89136c589b1bffd83332aae8eadf92e13777ee
Martin Mendez <133718278+Lmptincho@users.noreply.github.com>
2023-06-13 12:58:07 -0300

    Create Ejercicio09.js

---
+++ Modulo01/Tp1/Ejercicio09.js
@@ -0,0 +1,5 @@
+//Crear el archivo “ejercicio_9.js”. Declarar un array llamado “vocales” con las 5 letras. Luego se
+//deben imprimir en el navegador uno debajo de otro
+
+let vocales = `a <br/> e <br/> i <br/> o <br/> u`
+document.write(vocales)
